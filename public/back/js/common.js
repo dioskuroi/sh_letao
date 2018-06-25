8bb73e835fab7c898325394cd3ca88f679567e35
xujun <xujun1019@hotmail.com>
2018-06-25 19:20:10 +0800

    首页公共部分静态页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,8 +1,14 @@
+// 全局ajax进度条功能
 $($ => {
-  $(document).ajaxStart(()=>{
+  $(document).ajaxStart(() => {
     NProgress.start()
   })
-  $(document).ajaxStop(()=>{
+  $(document).ajaxStop(() => {
     NProgress.done()
   })
+})
+
+// 公共侧边栏和头部功能
+$($ => {
+  
 })
